e66ebc5d359597bba3f3a9b4f40bab326e62cc0f
CReinicke1992 <culeche@gmail.com>
2018-01-05 19:57:48 +0100

    added README.md

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,25 +1,31 @@
 # File: guessinggame.sh
 
-num_files=$(ls -1 | wc -l)
+function guess_func {	
+
+	local num_files=$(ls -1 | wc -l)		
+	
+	if [[ $2 -eq $num_files ]]
+        then
+                echo "Congratulations! Your guess is correct."
+		let $1=$1+1
+        elif [[ $2 -lt $num_files ]]
+        then
+                echo "Your guess is too low. Try again:"
+                read $2
+        elif [[ $2 -gt $num_files ]]
+        then
+                echo "Your guess is too high. Try again:"
+                read $2
+        fi
+}
+
 count=0
 
 echo "How many files are in the current directory?"
 read guess
 
 while [[ $count -eq 0 ]]
-do
-	if [[ $guess -eq $num_files ]]
-	then 
-		echo "Congratulations, your guess is correct."
-		let count=$count+1
-	elif [[ $guess -lt $num_files ]]
-	then 
-		echo "Your guess is too low. Try again."
-		read guess
-	elif [[ $guess -gt $num_files ]]
-	then
-		echo "Your guess is too high. Try again."
-		read guess
-	fi
+do	
+	guess_func count guess 
 done
 
